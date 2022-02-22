47b732ae0df8ad42d3777f017b7371861a555172
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-02-22 14:01:57 -0500

    updated nav and login

--- partials/nav.php
+++ partials/nav.php
@@ -1,75 +1,35 @@
 <?php
-require(__DIR__ . "/../../lib/functions.php");
-?>
-<form onsubmit="return validate(this)" method="POST">
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" required />
-    </div>
-    <div>
-        <label for="pw">Password</label>
-        <input type="password" id="pw" name="password" required minlength="8" />
-    </div>
-    <div>
-        <label for="confirm">Confirm</label>
-        <input type="password" name="confirm" required minlength="8" />
-    </div>
-    <input type="submit" value="Register" />
-</form>
-<script>
-    function validate(form) {
-        //TODO 1: implement JavaScript validation
-        //ensure it returns false for an error and true for success
-
-        return true;
-    }
-</script>
-<?php
-//TODO 2: add PHP Code
-if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
-    $email = se($_POST, "email", "", false);
-    $password = se($_POST, "password", "", false);
-    $confirm = se($_POST, "confirm", "", false);
-    $hasError = false;
-    if (empty($email)) {
-        echo "Email must not be empty";
-        $hasError = true;
-    }
-    //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email";
-        $hasError = true;
-    }
-    if (empty($password)) {
-        echo "Password must not be empty";
-        $hasError = true;
-    }
-    if (empty($confirm)) {
-        echo "Confirm Password must not be empty";
-        $hasError = true;
-    }
-    if (strlen($password) < 8) {
-        echo "Password must be >8 characters";
-        $hasError = true;
-    }
-    if (strlen($password) > 0 && $password !== $confirm) {
-        echo "Passwords must match";
-        $hasError = true;
-    }
-    if (!$hasError) {
-        //TODO 4
-        echo "Welcome, $email ";
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        $db = getDB();
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
-        try {
-            $stmt->execute([":email" => $email, ":password" => $hash]);
-            echo "Successfully registered!";
-        } catch (Exception $e) {
-            echo "There was a problem registering";
-            echo "<pre>" . var_export($e, true) . "</pre>";
-        }
-    }
+//Note: this is to resolve cookie issues with port numbers
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    $domain = explode(":", $domain)[0];
 }
+$localWorks = true; //some people have issues with localhost for the cookie params
+//if you're one of those people make this false
+
+//this is an extra condition added to "resolve" the localhost issue for the session cookie
+if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+    session_set_cookie_params([
+        "lifetime" => 60 * 60,
+        "path" => "/Project",
+        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+        "domain" => $domain,
+        "secure" => true,
+        "httponly" => true,
+        "samesite" => "lax"
+    ]);
+}
+session_start();
+//include functions here so we can have it on every page that uses the nav bar
+//that way we don't need to include so many other files on each page
+//nav will pull in functions and functions will pull in db
+require(__DIR__ . "/../lib/functions.php");
 ?>
+<nav>
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
