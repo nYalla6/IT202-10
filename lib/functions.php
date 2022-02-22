7c00239ecdf5d83e5aa93f497c12a699f8337abb
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-02-22 13:21:56 -0500

    Updated function

---
+++ lib/functions.php
@@ -0,0 +1,16 @@
+<?php
+//TODO 1: require db.php
+//include vs require: wants vs needs to run
+
+//__DIR__ gets the current php file gives the absolute path
+//and less likely to make mistakes
+require_once(__DIR__ . "/db.php");
+
+//require safer_echo.php
+require(__DIR__ . "/safer_echo.php");
+//TODO 2: filter helpers
+
+//TODO 3: User helpers
+
+//TODO 4: Flash Message Helpers
+?>
