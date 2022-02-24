3406f599376a933b1513ff4e27cb2c6a4537d6e0
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-02-24 13:55:21 -0500

    Editing the project pages with the new helper functions

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -3,10 +3,16 @@ require(__DIR__."/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
- echo "Welcome, " . $_SESSION["user"]["email"]; 
-}
-else{
-  echo "You're not logged in";
+// if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+//  echo "Welcome, " . $_SESSION["user"]["email"]; 
+// }
+// else{
+//   echo "You're not logged in";
+// }
+
+if (is_logged_in()) {
+  echo "welcome, " . get_user_email();
+} else {
+  echo "You're not logged in"; 
 }
 ?>
