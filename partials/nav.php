a7e31f1f6df1e9bc08dd0c8cabf94a56eacb41ac
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-03-22 13:50:25 -0400

    styles sheet

--- partials/nav.php
+++ partials/nav.php
@@ -23,6 +23,9 @@ session_start();
 require_once(__DIR__ . "/../lib/functions.php");
 
 ?>
+
+<link rel="stylesheet" href="styles.css">
+<script src = "helpers.js"></script>
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
