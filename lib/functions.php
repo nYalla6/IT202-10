10ea97f29f103a90d4f5f899eed6bae56ac53d96
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-03-22 13:34:42 -0400

    Update home and login

--- lib/functions.php
+++ lib/functions.php
@@ -5,6 +5,9 @@ require_once(__DIR__ . "/db.php");
 //This MUST match the folder name exactly
 $BASE_PATH = '/Project/';
 
+//TODO 4: Flash Message Helpers - moved so we don't get an exception
+require(__DIR__ . "/flash_messages.php");
+
 //require safer_echo.php
 require(__DIR__ . "/safer_echo.php");
 
@@ -13,9 +16,6 @@ require(__DIR__ . "/sanitizers.php");
 
 //TODO 3: User helpers
 require(__DIR__ . "/user_helpers.php");
-
-//TODO 4: Flash Message Helpers
-require(__DIR__ . "/flash_messages.php");
 
 //duplicate email/username
 require(__DIR__ . "/duplicate_user_details.php");
