a4f34a6521194b2ebf0801eb4de37ccd6c7bbe02
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-03-22 13:57:16 -0400

    Updated everything and cleaned up files

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -2,10 +2,15 @@
 require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
+
 <?php
-is_logged_in(true);
+
+if (is_logged_in(true)) {
+    //comment this out if you don't want to see the session variables
+    error_log("Session data: " . var_export($_SESSION, true));
+}
 ?>
 
-<?php 
+<?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
