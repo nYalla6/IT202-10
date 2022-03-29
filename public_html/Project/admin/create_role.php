5b158c61c7aea928fc7421ecd2f6321cfb90235e
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-03-29 14:20:16 -0400

    Updated nav, and admin folder content

--- public_html/Project/admin/create_role.php
+++ public_html/Project/admin/create_role.php
@@ -40,6 +40,7 @@ if (isset($_POST["name"]) && isset($_POST["description"])) {
     </div>
     <input type="submit" value="Create Role" />
 </form>
+
 <?php
 //note we need to go up 1 more directory
 require_once(__DIR__ . "/../../../partials/flash.php");
