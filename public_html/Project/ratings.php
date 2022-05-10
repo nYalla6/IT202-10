0bdf8d197c491bbbc05cf040da76fee46f21571c
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2022-05-10 02:16:29 -0400

    added verification for ratings

--- public_html/Project/ratings.php
+++ public_html/Project/ratings.php
@@ -1,45 +1,60 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
-//Rate button on Details.php redirect to ratings.php check
-//make html form for user check
-//check if user has purchased this item before in 
-//create php that will insert into ratings table 
-//in details .php select from the ratings table, and use for loop to print
-//average ratings
-//pagination
+
 $product_id = se($_GET, "id", -1, false);
 $user_id = get_user_id();
 
+
+
 $db = getDB();
-$stmt = $db->prepare("SELECT name, avg_rating, num_rating FROM Products where id = :id");
+//see if the user has purchases to allow them to rate
+$purchase = false;
+$stmt = $db->prepare("SELECT Orders.id FROM Orders INNER JOIN OrderItems ON OrderItems.order_id = Orders.id WHERE Orders.user_id = :user_id && OrderItems.product_id = :product_id");
 try {
-    $stmt->execute([":id" => $product_id]);
+    $stmt->execute([":user_id" => $user_id, ":product_id" => $id]);
     $r = $stmt->fetch(PDO::FETCH_ASSOC);
-    $name = se($r, "name", "", false);
-    $avg_rating = se($r, "avg_rating", 0.0, false);
-    $num_rating = se($r, "num_rating", 0, false);
-    
-    
+    if ($r) {
+        $purchase = true;
+    }
 } catch (PDOException $e) {
     error_log("<pre>" . var_export($e, true) . "</pre>");
 }
-
-if (isset($_POST["rating"]) && isset($_POST["review"])) {
-
-    $rating = se($_POST, "rating", -1, false);
-    $comment = se($_POST, "review", "", false);
-    //insert into table
-    $avg_rating = ((int)($avg_rating) + (int)($rating)) / 2;
-    add_rating($product_id, $user_id, $rating, $comment);
-    $stmt2 = $db->prepare("UPDATE Products SET avg_rating = $avg_rating, num_rating = num_rating+1 WHERE id= :product_id");
+if($purchase){
+    $stmt = $db->prepare("SELECT name, avg_rating, num_rating FROM Products where id = :id");
     try {
-        $stmt2->execute([":product_id" => $product_id]);
+        $stmt->execute([":id" => $product_id]);
+        $r = $stmt->fetch(PDO::FETCH_ASSOC);
+        $name = se($r, "name", "", false);
+        $avg_rating = se($r, "avg_rating", 0.0, false);
+        $num_rating = se($r, "num_rating", 0, false);
+        
+        
     } catch (PDOException $e) {
         error_log("<pre>" . var_export($e, true) . "</pre>");
     }
-    redirect('details.php?id=' . $product_id);
+    
+    if (isset($_POST["rating"]) && isset($_POST["review"])) {
+    
+        $rating = se($_POST, "rating", -1, false);
+        $comment = se($_POST, "review", "", false);
+        //insert into table
+        $avg_rating = ((int)($avg_rating) + (int)($rating)) / 2;
+        add_rating($product_id, $user_id, $rating, $comment);
+        $stmt2 = $db->prepare("UPDATE Products SET avg_rating = $avg_rating, num_rating = num_rating+1 WHERE id= :product_id");
+        try {
+            $stmt2->execute([":product_id" => $product_id]);
+        } catch (PDOException $e) {
+            error_log("<pre>" . var_export($e, true) . "</pre>");
+        }
+        redirect('details.php?id=' . $product_id);
+    }
 }
+else {
+    flash("You shall not pass! You didn't buy the item", "danger");
+    redirect("home.php");
+}
+
 ?>
 
 <div class="container-fluid">
